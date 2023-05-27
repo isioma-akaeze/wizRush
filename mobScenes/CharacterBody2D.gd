c787fc27638321e2529edd5f3d1458d446944bb0
isioma-akaeze <isiomarichardakaeze@gmail.com>
2023-05-27 01:59:21 -0600

    Added crouching, fixed the walk animation. Still need to work on collision and ladder functionality.

--- mobScenes/CharacterBody2D.gd
+++ mobScenes/CharacterBody2D.gd
@@ -4,6 +4,7 @@ onready var sprite := $Sprite
 var idle := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_stand.png")
 var jump := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_jump.png")
 var fall := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_fall.png")
+var crouch := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_duck.png")
 const GRAVITY := 300.0 #Rate at which the character vertically translate positively
 const WALK_SPEED := 200 #Rate at which the character horizontally translates 
 const velocity := Vector2.ZERO 
@@ -11,9 +12,27 @@ const JUMP_SPEED := -250.0 #Rate at which the character vertically translates ne
 var doubleJump := false #Can the character press the jump button again?
 onready var walk := $AnimationPlayer
 
+			
+
+
 func _physics_process(delta):
 	print("Double Jump Allowed?: " + str(doubleJump)) #Just some test outputs for Isioma
-	velocity.y += delta * GRAVITY #Every frame, remove a certain amound from the y value of velocity
+	print("Is on ceiling?: " + str(is_on_ceiling()))
+	if Input.is_action_pressed("left"):
+		if is_on_floor() and walk.current_animation != "walk":
+			if Input.is_action_pressed("down"):
+					walk.play("walk")
+	elif Input.is_action_pressed("right"):
+		if is_on_floor() and walk.current_animation != "walk" :
+				walk.play("walk")
+	if Input.is_action_just_released("left"):
+		walk.stop(false)
+	elif Input.is_action_just_released("right"):
+		walk.stop(false)
+	if is_on_floor():
+		velocity.y = 0 #Set the velocity to not change if I'm on the ground, otherwise if I slide off a collision box, the fall speed is way too fast (it's almost comical)
+	else:
+		velocity.y += delta * GRAVITY #Every frame, remove a certain amound from the y value of velocity
 	var direction := Input.get_axis("left", "right") #The direction variable is equal to either what the left or right key provides
 	if Input.is_action_pressed("left"):
 		sprite.flip_h = -1
@@ -23,6 +42,8 @@ func _physics_process(delta):
 		sprite.flip_h = 0
 		if is_on_floor():
 			walk.play("walk")
+
+	
 	if Input.is_action_just_pressed("up") and is_on_floor(): #"is_action_just_pressed" is faster than is "is_action_pressed, allowing enough time for a double jump to be allowed.
 		doubleJump = true #Allow a double jump
 		velocity.y = JUMP_SPEED #Add the JUMP_SPEED value as long as the if statement requirements are truee
@@ -46,6 +67,32 @@ func _physics_process(delta):
 	elif not Input.is_action_pressed("up") and !is_on_floor() and !doubleJump:
 		#PLACEHOLDER WAIT A CERTAIN AMOUNT OF SECONDS
 		sprite.set_texture(fall)
+	
+	if Input.is_action_pressed("down") and is_on_floor():
+		walk.stop(true)
+		$Crouch.disabled = false
+		$Fullbody.disabled = true
+		sprite.set_texture(crouch)
+		
+	elif !Input.is_action_pressed("down") and is_on_floor():
+		if !is_on_ceiling():
+			$Crouch.disabled = true
+			$Fullbody.disabled = false
+			sprite.set_texture(idle)
+		elif is_on_ceiling():
+			$Crouch.disabled = false
+			$Fullbody.disabled = true
+			sprite.set_texture(crouch)
+		
+	elif !Input.is_action_pressed("down") and !is_on_floor():
+		if !is_on_ceiling():
+			$Crouch.disabled = true
+			$Fullbody.disabled = false
+			sprite.set_texture(fall)
+		elif is_on_ceiling():
+			$Crouch.disabled = false
+			$Fullbody.disabled = true
+			sprite.set_texture(crouch)
 	
 	velocity.x = direction * WALK_SPEED #Every frame, add to the x value of velocity the direction value multiplied by walk speed
 	move_and_slide(velocity, Vector2.UP) #Here I added "Vector2.UP" because otherwise "is_on_floor() literally would not work. 
