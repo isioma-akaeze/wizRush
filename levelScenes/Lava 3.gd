e4c6fde8d57cdc85c30f0141f38168bbb040d489
isioma-akaeze <isiomarichardakaeze@gmail.com>
2023-05-27 13:25:32 -0600

    Added some shrubs, expanded the level a bit.

--- levelScenes/Lava 3.gd
+++ levelScenes/Lava 3.gd
@@ -1,12 +1,7 @@
 extends Area2D
 
-
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body: Node):
 	get_tree().quit()
-
-
-
-
--- mobScenes/CharacterBody2D.gd
+++ mobScenes/CharacterBody2D.gd
@@ -5,32 +5,39 @@ var idle := preload("res://assets/images/kenney_platformer-characters/PNG/Advent
 var jump := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_jump.png")
 var fall := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_fall.png")
 var crouch := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_duck.png")
+var climb := preload("res://assets/images/kenney_platformer-characters/PNG/Adventurer/Poses/adventurer_climb1.png")
 const GRAVITY := 300.0 #Rate at which the character vertically translate positively
+const CLIMB_GRAVITY := 100.0
 const WALK_SPEED := 200 #Rate at which the character horizontally translates 
 const velocity := Vector2.ZERO 
 const JUMP_SPEED := -250.0 #Rate at which the character vertically translates negatively
 var doubleJump := false #Can the character press the jump button again?
 onready var walk := $AnimationPlayer
-
+export var climbing = false
 
 func _physics_process(delta):
-	print("Double Jump Allowed?: " + str(doubleJump)) #Just some test outputs for Isioma
-	print("Is on ceiling?: " + str(is_on_ceiling()))
+	print("Climbing? " + str(climbing))
 	if Input.is_action_pressed("left"):
-		if is_on_floor() and walk.current_animation != "walk":
+		if is_on_floor() and walk.current_animation != "walk" and !is_on_wall():
 			if Input.is_action_pressed("down"):
 					walk.play("walk")
 	elif Input.is_action_pressed("right"):
-		if is_on_floor() and walk.current_animation != "walk" :
+		if is_on_floor() and walk.current_animation != "walk" and !is_on_wall():
 				walk.play("walk")
 	if Input.is_action_just_released("left"):
 		walk.stop(false)
 	elif Input.is_action_just_released("right"):
 		walk.stop(false)
+	if is_on_wall() or is_on_ceiling():
+		walk.stop(true)
 	if is_on_floor():
 		velocity.y = 0 #Set the velocity to not change if I'm on the ground, otherwise if I slide off a collision box, the fall speed is way too fast (it's almost comical)
 	else:
-		velocity.y += delta * GRAVITY #Every frame, remove a certain amound from the y value of velocity
+		if climbing == true and Input.is_action_pressed("down"):
+			sprite.set_texture(climb)
+			velocity.y -= delta * CLIMB_GRAVITY
+		else:
+			velocity.y += delta * GRAVITY #Every frame, remove a certain amound from the y value of velocity
 	var direction := Input.get_axis("left", "right") #The direction variable is equal to either what the left or right key provides
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("up"):
 		sprite.flip_h = -1
@@ -60,12 +67,12 @@ func _physics_process(delta):
 	elif is_on_floor() and not Input.is_action_just_pressed("up") and !doubleJump:
 		sprite.set_texture(idle)
 	
-	elif not Input.is_action_pressed("up") and !is_on_floor() and doubleJump:
+	elif not Input.is_action_pressed("up") and !is_on_floor() and doubleJump and climbing != true:
 		#PLACEHOLDER WAIT A CERTAIN AMOUNT OF SECONDS
 		walk.stop(true)
 		sprite.set_texture(fall)
 	
-	elif not Input.is_action_pressed("up") and !is_on_floor() and !doubleJump:
+	elif not Input.is_action_pressed("up") and !is_on_floor() and !doubleJump and climbing != true:
 		#PLACEHOLDER WAIT A CERTAIN AMOUNT OF SECONDS
 		walk.stop(true)
 		sprite.set_texture(fall)
@@ -99,6 +106,5 @@ func _physics_process(delta):
 	
 	velocity.x = direction * WALK_SPEED #Every frame, add to the x value of velocity the direction value multiplied by walk speed
 	move_and_slide(velocity, Vector2.UP) #Here I added "Vector2.UP" because otherwise "is_on_floor() literally would not work. 
-	print("Player Touching Ground?: " + str(is_on_floor())) # Just some test outputs for Isioma
 
 
