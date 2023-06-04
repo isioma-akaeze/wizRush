9ca6966a68a4120d50d829d70daa3e16a27b3bdc
isioma-akaeze <isiomarichardakaeze@gmail.com>
2023-06-03 21:34:11 -0600

    Changed some settings for gravity and added a spider mob that walks around normally but follows the player within sight range.

---
+++ mobScenes/Spider.gd
@@ -0,0 +1,61 @@
+extends KinematicBody2D
+
+const GRAVITY := 10500.0
+const JUMP_SPEED := -540.0
+export var switchingDirection := false
+var targetAcquired := false
+var hasToJump := false
+var speed := 35.0 
+onready var animation := $AnimationPlayer
+var direction := Vector2(-3,0)
+var bodyX : Node = null
+onready var sprite := $Sprite
+onready var idle := preload("res://assets/images/Extra animations and enemies/Enemy sprites/spider.png")
+
+func _physics_process(delta):
+	if targetAcquired:
+		speed = 105.0
+		direction = global_position.direction_to(bodyX.global_position)
+		direction.y = 0
+		if direction.x >= 0:
+			sprite.flip_h = -1
+		elif direction.x < 0:
+			sprite.flip_h = 0
+		elif round(direction.x) == 0:
+			sprite.flip_h = -1
+		
+	elif switchingDirection and !targetAcquired:
+		speed = 35.0
+		direction = Vector2(3, 0)
+		sprite.flip_h = -1
+	elif !switchingDirection and !targetAcquired:
+		speed = 35.0
+		direction = Vector2(-3, 0)
+		sprite.flip_h = 0
+	
+	var velocity := direction * speed
+	print(velocity.x)
+	if !is_on_floor() and !hasToJump:
+		velocity.y += delta * GRAVITY
+	elif hasToJump:
+		velocity.y -= JUMP_SPEED
+	if round(velocity.x) == 0:
+			animation.stop()
+			sprite.set_texture(idle)
+	else:
+		animation.play("walk")
+	move_and_slide(velocity, Vector2.UP)
+	
+
+func _on_EngageArea_body_entered(body):
+	if body.is_in_group("player"):
+		if body.position.y <= global_position.y:
+			targetAcquired = true
+			bodyX = body
+			return bodyX
+		elif body.position.y > self.position.y:
+			targetAcquired = false
+
+func _on_EngageArea_body_exited(body):
+	if body.is_in_group("player"):
+		targetAcquired = false
