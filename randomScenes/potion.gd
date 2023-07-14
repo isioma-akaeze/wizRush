d4fb73b15fa3def03b60f6a479ef86676633527f
isioma-akaeze <isiomarichardakaeze@gmail.com>
2023-07-13 21:54:16 -0600

    Added difficulty to potions and fixed the bug where the player could receive potions upon death.

--- randomScenes/potion.gd
+++ randomScenes/potion.gd
@@ -1,16 +1,27 @@
 extends Area2D
 onready var animation := $AnimationPlayer
+onready var difficulty := get_node("/root/GlobalOptionButton")
 
 func _process(delta):
 	animation.play("New Anim")
 
 func _on_Potion_body_entered(body):
 	if body.is_in_group("player"):
-		if body.health <= 75:
-			body.health += 25
-			queue_free()
-		elif body.health > 75 and body.health < 100:
-			body.health += (100 - body.health)
-			queue_free()
-		elif body.health >= 100:
-			pass
+		if difficulty.difficulty == 0:
+			if body.health <= 75 and body.health > 0:
+				body.health += 25
+				queue_free()
+			elif body.health > 75 and body.health < 100:
+				body.health += (100 - body.health)
+				queue_free()
+			elif body.health >= 100:
+				pass
+		elif difficulty.difficulty == 1:
+			if body.health <= 85 and body.health > 0:
+				body.health += 15
+				queue_free()
+			elif body.health > 85 and body.health < 100:
+				body.health += (100 - body.health)
+				queue_free()
+			elif body.health >= 100:
+				pass
