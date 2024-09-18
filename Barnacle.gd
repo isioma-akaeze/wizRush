686514884d6cd598043bce1ff49acbd9d9400cbd
isioma-akaeze <132284922+isioma-akaeze@users.noreply.github.com>
2024-09-18 13:35:38 -0600

    Prematurely started code for a new boss attack, and changed spawn rates for piranhas and fish from the boss.
    Need to work on the new vomit attack for the boss and finish it.

--- Barnacle.gd
+++ Barnacle.gd
@@ -39,16 +39,40 @@ func _on_GeneralAnimation_animation_finished(anim_name):
 			piranhaScene = load("res://mobScenes/Piranha.tscn")
 		var xOffset := 0
 		var spawns := 0
-		for i in range(2):
-			var piranhaSceneInstance : KinematicBody2D = piranhaScene.instance()
-			var piranhaX := self.position.x - rand_range(-20, 23.0) +  xOffset
-			var piranhaY := self.position.y - rand_range(100.0, 320.0)
-			piranhaSceneInstance.position = Vector2(piranhaX, piranhaY)
-			get_tree().current_scene.add_child(piranhaSceneInstance)
-			xOffset += 120
-			spawns += 1
+		if randomMob == 0:
+			for i in range(2):
+				var piranhaSceneInstance : KinematicBody2D = piranhaScene.instance()
+				var piranhaX := self.position.x - rand_range(-20, 23.0) +  xOffset
+				var piranhaY := self.position.y - rand_range(100.0, 320.0)
+				piranhaSceneInstance.position = Vector2(piranhaX, piranhaY)
+				get_tree().current_scene.add_child(piranhaSceneInstance)
+				xOffset += 120
+				spawns += 1
+		elif randomMob == 1:
+			for i in range(1):
+				var piranhaSceneInstance : KinematicBody2D = piranhaScene.instance()
+				var piranhaX := self.position.x - rand_range(-20, 23.0) +  xOffset
+				var piranhaY := self.position.y - rand_range(100.0, 320.0)
+				piranhaSceneInstance.position = Vector2(piranhaX, piranhaY)
+				get_tree().current_scene.add_child(piranhaSceneInstance)
+				xOffset += 120
+				spawns += 2
 		if spawns == 2:
 			animation.play("bite")
+	#elif anim_name == "vomit":
+		# var vomitBallScene : PackedScence = null
+		# var vomits := 0
+		# vomitBallScene = load("res://VomitBall.tscn")
+		# for i in range(3):
+			#var vomitBallInstance : KinematicBody2D = vomitBallScence.instance()
+			#var vomitX := self.position.x 
+			#var vomitY := self.position.y - 70
+			#vomitBallInstance.position = Vector2(vomitX, vomitY)
+			#get_tree().current_scene.add_child(piranhaSceneInstance) 
+			#vomits += 1
+		#if vomits == 3:
+			#animation.play("bite")
+			
 
 func _on_BubbleArea_body_entered(body):
 	if body.is_in_group("player"):
@@ -80,8 +104,10 @@ func _on_BubbleArea_body_exited(body):
 			damageTimer.stop()
 
 func _on_TeethArea_body_entered(body):
-	if body.is_in_group("player") or body.is_in_group("enemy"):
+	if body.is_in_group("player"):
 		body.health -= 100
+	elif body.is_in_group("enemy"):
+		body.health -= 15
 
 func _on_DamageTimerBoss_timeout():
 	if difficulty.difficulty == 0:
@@ -100,6 +126,8 @@ func _randomAttack():
 		bubblesAnimation.play("bubbles")
 	elif randomAttack == 1:
 		animation.play("spit")
+	#elif randomAttack == 3:
+		#animation.play("vomit")
 
 func _on_AttackWaitTimer_timeout():
 	_randomAttack()
